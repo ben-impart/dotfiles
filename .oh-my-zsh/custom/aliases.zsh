5d7c0ba7dd9c36741ddcd182cb4ffb9f2a3b54ff
Ben Pinkerton <ben@impart.security>
2023-02-15 15:50:23 -0600

    Move zsh aliases to .oh-my-zsh/custom

---
+++ .oh-my-zsh/custom/aliases.zsh
@@ -0,0 +1,14 @@
+# Git aliases
+alias g='git status'
+alias gs='git status'
+alias gl='git log'
+alias ga='git add'
+alias gaa='git add .'
+alias gco='git checkout'
+alias gb='git branch'
+alias gc='git commit'
+alias gd='git diff'
+alias gds='git diff --staged'
+
+# Other
+alias nr="npm run"
